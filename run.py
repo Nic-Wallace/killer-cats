0f22c85337a67c39d52b512d83502756fb5f9313
Nic-Wallace <nfewallace@gmail.com>
2023-06-19 13:58:16 +0000

    Add comments to functions

--- run.py
+++ run.py
@@ -3,6 +3,10 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 def try_again(): 
+    """
+    Asks the user if they want to try the game again.
+    Sends them back to the start or closes the program.
+    """
     answer = input("Would you like to try again? (yes/no): ")
     if answer.lower().strip() == "y":
         intro()
@@ -10,12 +14,18 @@ def try_again():
         pass
 
 def step_one():
+    """
+    First step of the game from going down the correct path.
+    """
     print("You sprint back, away from the growling.")
     print("Stumbling through the forest, you find a small dirt path.")
     print("It leads left, and right.")
     answer = input("Which way do you go? (left/right)")
 
 def intro():
+    """
+    Called to start the game, gives the first question.
+    """
     print("You wake up, surrounded by trees and very confused.")
     print('"How did I get here?"')
     print("Your head hurts, you\'re covered in scratches.")
